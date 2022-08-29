42d3ca162a9f7318e9b513bdbff188bb600da8d1
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-08-29 20:17:36 +0530

    .

---
+++ Database/db.js
@@ -0,0 +1,15 @@
+const mongoose=require("mongoose");
+async function connectDB(){
+    return new promise((resolve,reject)=>{
+        const url='mongodb+srv://sam:sam@moviedb.gseux4l.mongodb.net/?retryWrites=true&w=majority';
+        mongoose.connect(url,(error)=>{
+            if(error){
+                console.error('error connecting to db...',error);
+                return reject(error)
+            }
+
+            console.log('connected to DB');
+            resolve();
+        })
+    })
+}
---
+++ Database/index.js
@@ -0,0 +1,5 @@
+const express=require('express');
+const connectDB=require("./db");
+const app=express();
+
+app.use(express.json());
